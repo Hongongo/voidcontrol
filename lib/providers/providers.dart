0962cc181ee8026c748822738a57c9f46c4dd1fc
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-17 15:11:10 -0600

    added categories provider

---
+++ lib/providers/providers.dart
@@ -0,0 +1,6 @@
+export '../../providers/users_provider.dart';
+export '../../providers/user_form_provider.dart';
+export '../../providers/sidemenu_provider.dart';
+export '../../providers/register_form_provider.dart';
+export '../../providers/categories_provider.dart';
+export '../../providers/auth_provider.dart';
