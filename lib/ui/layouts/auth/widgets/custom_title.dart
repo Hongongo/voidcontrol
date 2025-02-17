9039b30bacbb74fc44825622207b6228a9788f22
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-17 00:00:20 -0600

    center image in custom title

--- lib/ui/layouts/auth/widgets/custom_title.dart
+++ lib/ui/layouts/auth/widgets/custom_title.dart
@@ -5,27 +5,26 @@ class CustomTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric( horizontal: 20 ),
+      padding: EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.asset(
-            'assets/logo.png',
-            width: 50,
-            height: 50,
+          Center(
+            child: Image.asset(
+              'assets/logo.png',
+              width: 50,
+              height: 50,
+            ),
           ),
-
-          SizedBox( height: 20 ),
-
+          SizedBox(height: 20),
           FittedBox(
             fit: BoxFit.contain,
             child: Text(
               'A call to the void...',
               style: GoogleFonts.montserratAlternates(
-                fontSize: 60,
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-              ),
+                  fontSize: 60,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
             ),
           )
         ],
