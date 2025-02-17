6d7c78603e5051f628c48187dd911c71fdd41445
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-16 23:13:55 -0600

    added admin handlers

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,19 @@ import 'package:admin_dashboard/ui/layouts/splash/splash_layout.dart';
 
 import 'package:admin_dashboard/router/router.dart';
 
-
 import 'package:admin_dashboard/providers/providers.dart';
-
 
 import 'package:admin_dashboard/services/local_storage.dart';
 import 'package:admin_dashboard/services/navigation_service.dart';
 import 'package:admin_dashboard/services/notifications_service.dart';
 
 import 'package:admin_dashboard/ui/layouts/auth/auth_layout.dart';
- 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LocalStorage.configurePrefs();
   CafeApi.configureDio();
-  
+
   Flurorouter.configureRoutes();
   runApp(AppState());
 }
@@ -32,12 +30,11 @@ class AppState extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(lazy: false, create: ( _ ) => AuthProvider() ),
-        ChangeNotifierProvider(lazy: false, create: ( _ ) => SideMenuProvider() ),
-        ChangeNotifierProvider(create: ( _ ) => CategoriesProvider() ),
-        ChangeNotifierProvider(create: ( _ ) => UsersProvider() ),
-        ChangeNotifierProvider(create: ( _ ) => UserFormProvider() ),
-
+        ChangeNotifierProvider(lazy: false, create: (_) => AuthProvider()),
+        ChangeNotifierProvider(lazy: false, create: (_) => SideMenuProvider()),
+        ChangeNotifierProvider(create: (_) => CategoriesProvider()),
+        ChangeNotifierProvider(create: (_) => UsersProvider()),
+        ChangeNotifierProvider(create: (_) => UserFormProvider()),
       ],
       child: MyApp(),
     );
@@ -54,25 +51,22 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: Flurorouter.router.generator,
       navigatorKey: NavigationService.navigatorKey,
       scaffoldMessengerKey: NotificationsService.messengerKey,
-      builder: ( _ , child ){
+      builder: (_, child) {
         final authProvider = Provider.of<AuthProvider>(context);
 
-        if ( authProvider.authStatus == AuthStatus.checking )
+        if (authProvider.authStatus == AuthStatus.checking)
           return SplashLayout();
 
-        if( authProvider.authStatus == AuthStatus.authenticated ) {
-          return DashboardLayout( child: child! );
+        if (authProvider.authStatus == AuthStatus.authenticated) {
+          return DashboardLayout(child: child!);
         } else {
-          return AuthLayout( child: child! );
+          return AuthLayout(child: child!);
         }
       },
       theme: ThemeData.light().copyWith(
-        scrollbarTheme: ScrollbarThemeData().copyWith(
-          thumbColor: WidgetStateProperty.all(
-            Colors.grey.withOpacity(0.5)
-          )
-        )
-      ),
+          scrollbarTheme: ScrollbarThemeData().copyWith(
+              thumbColor:
+                  WidgetStateProperty.all(Colors.grey.withOpacity(0.5)))),
     );
   }
 }
