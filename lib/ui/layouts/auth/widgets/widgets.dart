b835a766bb282f859c2294f8fcd4f49bdfd82c85
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-17 12:40:33 -0600

    adds dashbord sidebar

---
+++ lib/ui/layouts/auth/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'package:admin_dashboard/ui/layouts/auth/widgets/background_logo.dart';
+export 'package:admin_dashboard/ui/layouts/auth/widgets/links_bar.dart';
+export 'package:admin_dashboard/ui/layouts/auth/widgets/custom_title.dart';
